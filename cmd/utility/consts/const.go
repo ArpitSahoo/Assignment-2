d61185c07b0167744fa990dd3412fc8b0394b5fe
bjornivar <bihoie@stud.ntnu.no>
2026-03-23 09:07:08 +0100

    add API endpoint constants

---
+++ cmd/utility/consts/const.go
@@ -0,0 +1,7 @@
+package consts
+
+const RestCountriesAPI = "http://129.241.150.113:8080/v3.1/all?fields=name"
+const CurrencyAPI = "http://129.241.150.113:9090/currency/NOK/"
+const MeteoAPI = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m"
+const OpenAQAPI = "https://api.openaq.org/v3/parameters/2"
+const NominatimAPI = "https://nominatim.openstreetmap.org/search?q=Oslo&format=jsonv2&limit=1"
