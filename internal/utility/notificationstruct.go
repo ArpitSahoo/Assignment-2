73b4d192a1686c977357df59f84acdaafb19261e
bjornivar <bihoie@stud.ntnu.no>
2026-04-01 21:01:41 +0200

    added structs to use for notificationhandler.go

---
+++ internal/utility/notificationstruct.go
@@ -0,0 +1,18 @@
+package utility
+
+type RegisterWebhook struct {
+	Url     string `json:"url"`
+	Country string `json:"country"`
+	Event   string `json:"event"`
+}
+
+type Threshold struct {
+	Url       string `json:"url"`
+	Country   string `json:"country"`
+	Event     string `json:"event"`
+	Threshold struct {
+		Field    string  `json:"field"`
+		Operator string  `json:"operator"`
+		Value    float64 `json:"value"`
+	} `json:"threshold"`
+}
