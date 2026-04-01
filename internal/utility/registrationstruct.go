136b3f5dac33ab28ce9b9bfcbba140ec5b9f59dd
EiLi1 <183522504+eili1s@users.noreply.github.com.>
2026-04-01 02:36:20 +0200

    initial commit
    added structs to use for registrationhandler.go

---
+++ internal/utility/registrationstruct.go
@@ -0,0 +1,29 @@
+package utility
+
+// RegistrationRequest represents a JSON payload used to create or replace
+// a dashboard configuration through the /registrations endpoint.
+type RegistrationRequest struct {
+	Country  string               `json:"country"`
+	IsoCode  string               `json:"isoCode"`
+	Features RegistrationFeatures `json:"features"`
+}
+
+// RegistrationFeatures specifies which dashboard fields to be included
+// in the dashboard configuration stored through the /registration endpoint.
+type RegistrationFeatures struct {
+	Temperature      bool     `json:"temperature"`
+	Precipitation    bool     `json:"precipitation"`
+	AirQuality       bool     `json:"airQuality"`
+	Capital          bool     `json:"capital"`
+	Coordinates      bool     `json:"coordinates"`
+	Population       bool     `json:"population"`
+	Area             bool     `json:"area"`
+	TargetCurrencies []string `json:"targetCurrencies"`
+}
+
+// RegistrationResponse is a JSON response returned by the /registration endpoint
+// after a registration has been created or updated.
+type RegistrationResponse struct {
+	Id         string `json:"id"`
+	LastChange string `json:"lastChange"`
+}
