14dd8786f507b77ba0ce69e4bb52986199714c5a
bjornivar <bihoie@stud.ntnu.no>
2026-04-02 21:34:18 +0200

    add constants for notification paths and webhook URL

--- internal/utility/constants.go
+++ internal/utility/constants.go
@@ -3,3 +3,9 @@ package utility
 const RegistrationsCollection = "registrations"
 
 const RegistrationPath = "/envdash/v1/registrations/"
+
+const NotificationPath = "/envdash/v1/notifications/{id}"
+
+const NotificationPathBase = "/envdash/v1/notifications/"
+
+const WebhookURL = "https://webhook.site/"
