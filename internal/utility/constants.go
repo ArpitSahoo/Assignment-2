8794df1bbfbd72adbda92bd761891bf15e65b478
bjornivar <bihoie@stud.ntnu.no>
2026-04-02 21:42:19 +0200

    remove WebhookURL constant from constants.go

--- internal/utility/constants.go
+++ internal/utility/constants.go
@@ -8,8 +8,6 @@ const NotificationPath = "/envdash/v1/notifications/{id}"
 
 const NotificationPathBase = "/envdash/v1/notifications/"
 
-const WebhookURL = "https://webhook.site/"
-
 const ContentType = "Content-Type"
 
 const ApplicationJSON = "application/json"
