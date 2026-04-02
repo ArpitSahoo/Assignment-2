67a1570b8480ed37a477224bff0d037cbcbb332e
bjornivar <bihoie@stud.ntnu.no>
2026-04-02 23:24:48 +0200

    refactor RegisterWebhook struct to include Threshold field for enhanced webhook configuration

--- internal/utility/notificationstruct.go
+++ internal/utility/notificationstruct.go
@@ -1,19 +1,15 @@
 package utility
 
 type RegisterWebhook struct {
-	ID      string `json:"id,omitempty"`
-	Url     string `json:"url"`
-	Country string `json:"country"`
-	Event   string `json:"event"`
+	ID        string     `json:"id,omitempty"`
+	Url       string     `json:"url"`
+	Country   string     `json:"country"`
+	Event     string     `json:"event"`
+	Threshold *Threshold `json:"threshold,omitempty"`
 }
 
 type Threshold struct {
-	Url       string `json:"url"`
-	Country   string `json:"country"`
-	Event     string `json:"event"`
-	Threshold struct {
-		Field    string  `json:"field"`
-		Operator string  `json:"operator"`
-		Value    float64 `json:"value"`
-	} `json:"threshold"`
+	Field    string  `json:"field"`
+	Operator string  `json:"operator"`
+	Value    float64 `json:"value"`
 }
