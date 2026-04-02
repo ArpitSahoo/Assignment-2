76ab1bb110b3adae1bb954b39743567b913cf20a
bjornivar <bihoie@stud.ntnu.no>
2026-04-02 21:34:30 +0200

    add ID field to RegisterWebhook struct for improved webhook registration

--- internal/utility/notificationstruct.go
+++ internal/utility/notificationstruct.go
@@ -1,6 +1,7 @@
 package utility
 
 type RegisterWebhook struct {
+	ID      string `json:"id,omitempty"`
 	Url     string `json:"url"`
 	Country string `json:"country"`
 	Event   string `json:"event"`
