de7f82b9905a3498108b08bba5eae367270cb05c
bjornivar <bihoie@stud.ntnu.no>
2026-04-03 15:08:10 +0200

    add WebhookResponse struct and update RegisterWebhook ID field to be non-omittable

--- internal/utility/notificationstruct.go
+++ internal/utility/notificationstruct.go
@@ -1,13 +1,20 @@
 package utility
 
+// RegisterWebhook represents a JSON payload used to create a webhook through the /notifications endpoint.
 type RegisterWebhook struct {
-	ID        string     `json:"id,omitempty"`
+	ID        string     `json:"id"`
 	Url       string     `json:"url"`
 	Country   string     `json:"country"`
 	Event     string     `json:"event"`
 	Threshold *Threshold `json:"threshold,omitempty"`
 }
 
+// WebhookResponse is a JSON response returned by the /notifications endpoint
+type WebhookResponse struct {
+	ID string `json:"id"`
+}
+
+// Threshold represents the threshold condition for triggering a webhook notification.
 type Threshold struct {
 	Field    string  `json:"field"`
 	Operator string  `json:"operator"`
