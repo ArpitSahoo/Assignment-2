0717b9c1fa507503c352082a58f6569f375ddb31
EiLi1 <183522504+eili1s@users.noreply.github.com.>
2026-04-04 18:03:22 +0200

    add tests for addRegistration, replaceRegistration and deleteRegistration; Changed third-party /assert import.

--- internal/handlers/registrationhandler_test.go
+++ internal/handlers/registrationhandler_test.go
@@ -4,6 +4,7 @@ import (
 	"assignment-2/internal/utility"
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +12,7 @@ import (
 	"testing"
 
 	"cloud.google.com/go/firestore"
-	"github.com/go-jose/go-jose/v4/testutils/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHandleAllGetRegistrations(t *testing.T) {
@@ -50,7 +51,7 @@ func TestHandleAllGetRegistrationsBadRequestTooLong(t *testing.T) {
 	}
 }
 
-func TestAddRegistration(t *testing.T) {
+func TestAddRegistrationSuccess(t *testing.T) {
 	origin := addRegistrationDocImpl
 	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
 		return "mock-id-001", nil
@@ -105,7 +106,7 @@ func TestAddRegistration(t *testing.T) {
 	assert.Equal(t, "mock-id-001", got.ID)
 }
 
-func TestAddRegistrationFalseFields(t *testing.T) {
+func TestAddRegistrationFalseFieldsSuccess(t *testing.T) {
 	origin := addRegistrationDocImpl
 	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
 		return "mock-id-001", nil
@@ -160,7 +161,7 @@ func TestAddRegistrationFalseFields(t *testing.T) {
 	assert.Equal(t, "mock-id-001", got.ID)
 }
 
-func TestAddRegistrationEmptyFeatures(t *testing.T) {
+func TestAddRegistrationEmptyFeaturesSuccess(t *testing.T) {
 	origin := addRegistrationDocImpl
 	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
 		return "mock-id-001", nil
@@ -206,7 +207,7 @@ func TestAddRegistrationEmptyFeatures(t *testing.T) {
 	assert.Equal(t, "mock-id-001", got.ID)
 }
 
-func TestAddRegistrationNormalization(t *testing.T) {
+func TestAddRegistrationNormalizationSuccess(t *testing.T) {
 	origin := addRegistrationDocImpl
 	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
 		return "mock-id-001", nil
@@ -261,7 +262,7 @@ func TestAddRegistrationNormalization(t *testing.T) {
 	assert.Equal(t, "mock-id-001", got.ID)
 }
 
-func TestAddRegistrationEmptyTargetCurrencies(t *testing.T) {
+func TestAddRegistrationEmptyTargetCurrenciesSuccess(t *testing.T) {
 	origin := addRegistrationDocImpl
 	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
 		return "mock-id-001", nil
@@ -620,7 +621,7 @@ func TestAddRegistrationInvalidCountry3(t *testing.T) {
 	assert.Equal(t, before, after)
 }
 
-func TestAddRegistrationInvalidJson(t *testing.T) {
+func TestAddRegistrationInvalidJson1(t *testing.T) {
 	h := &Handler{}
 	// removed brackets for features
 	body := `{
@@ -732,4 +733,629 @@ func TestAddRegistrationInvalidJson3(t *testing.T) {
 	}(resp.Body)
 	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	assert.Equal(t, before, after)
+}
+
+func TestAddRegistrationServerError(t *testing.T) {
+	origin := addRegistrationDocImpl
+	addRegistrationDocImpl = func(ctx context.Context, client *firestore.Client, reg map[string]any) (string, error) {
+		return "", errors.New("failed to add registration")
+	}
+
+	defer func() {
+		addRegistrationDocImpl = origin
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": "Norway",
+   		"isoCode": "NO",
+   		"features": {
+      		"temperature": true,
+      		"precipitation": true,
+      		"airQuality": true,
+      		"capital": true,
+      		"coordinates": true,
+      		"population": true,
+      		"area": true,
+      		"targetCurrencies": ["EUR", "USD", "SEK"]
+		}
+	}`
+	req := httptest.NewRequest(http.MethodPost, "/envdash/v1/registrations", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	before := h.RegistrationCount.Load()
+
+	h.addRegistration(w, req)
+
+	after := h.RegistrationCount.Load()
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	assert.Equal(t, before, after)
+}
+
+func TestReplaceRegistrationSuccess(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var gotID string
+	var gotReg map[string]any
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		gotID = id
+		gotReg = reg
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": "Norway",
+   		"isoCode": "NO",
+   		"features": {
+      		"temperature": true,
+      		"precipitation": true,
+      		"airQuality": true,
+      		"capital": true,
+      		"coordinates": true,
+      		"population": true,
+      		"area": true,
+      		"targetCurrencies": ["EUR", "USD", "SEK"]
+		}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "mock-id-001", gotID)
+	assert.Equal(t, "Norway", gotReg["country"])
+	assert.Equal(t, "NO", gotReg["isoCode"])
+	assert.NotEmpty(t, gotReg["lastChange"])
+
+	features, ok := gotReg["features"].(utility.RegistrationFeatures)
+	assert.True(t, ok)
+
+	assert.True(t, features.Temperature)
+	assert.True(t, features.Precipitation)
+	assert.True(t, features.AirQuality)
+	assert.True(t, features.Capital)
+	assert.True(t, features.Coordinates)
+	assert.True(t, features.Population)
+	assert.True(t, features.Area)
+	assert.Equal(t, []string{"EUR", "USD", "SEK"}, features.TargetCurrencies)
+}
+
+func TestReplaceRegistrationNormalizationSuccess(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var gotID string
+	var gotReg map[string]any
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		gotID = id
+		gotReg = reg
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {
+      		"temperature": true,
+      		"precipitation": true,
+      		"airQuality": true,
+      		"capital": true,
+      		"coordinates": true,
+      		"population": true,
+      		"area": true,
+      		"targetCurrencies": [" EUR", "UsD", "SEk "]
+		}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "mock-id-001", gotID)
+	assert.Equal(t, "Norway", gotReg["country"])
+	assert.Equal(t, "NO", gotReg["isoCode"])
+	assert.NotEmpty(t, gotReg["lastChange"])
+
+	features, ok := gotReg["features"].(utility.RegistrationFeatures)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"EUR", "USD", "SEK"}, features.TargetCurrencies)
+}
+
+func TestReplaceRegistrationEmptyFeaturesSuccess(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var gotID string
+	var gotReg map[string]any
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		gotID = id
+		gotReg = reg
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "mock-id-001", gotID)
+	assert.Equal(t, "Norway", gotReg["country"])
+	assert.Equal(t, "NO", gotReg["isoCode"])
+	assert.NotEmpty(t, gotReg["lastChange"])
+	features, ok := gotReg["features"].(utility.RegistrationFeatures)
+	assert.True(t, ok)
+	// Features should be stored as zero-value struct
+	assert.False(t, features.Temperature)
+	assert.False(t, features.Precipitation)
+	assert.False(t, features.AirQuality)
+	assert.False(t, features.Capital)
+	assert.False(t, features.Coordinates)
+	assert.False(t, features.Population)
+	assert.False(t, features.Area)
+	assert.Empty(t, features.TargetCurrencies)
+}
+
+func TestReplaceRegistrationInvalidJson(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var getCalled bool
+	var setCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		getCalled = true
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		setCalled = true
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+	// Removed last object boundary
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.False(t, getCalled)
+	assert.False(t, setCalled)
+}
+
+func TestReplaceRegistrationNotFound(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var setCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return errors.New("not found")
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		setCalled = true
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	assert.False(t, setCalled)
+}
+
+func TestReplaceRegistrationUpdateFailure(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		return errors.New("update failed")
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "mock-id-001")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+}
+
+func TestReplaceRegistrationInvalidId(t *testing.T) {
+	originGet := getRegistrationDoc
+	originSet := setRegistrationDoc
+
+	var gotID string
+	var setCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotID = id
+		return nil
+	}
+
+	setRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string, reg map[string]any) error {
+		setCalled = true
+		gotID = id
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		setRegistrationDoc = originSet
+	}()
+
+	h := &Handler{}
+
+	body := `{
+   		"country": " Norway",
+   		"isoCode": "No ",
+   		"features": {}
+	}`
+
+	req := httptest.NewRequest(http.MethodPut, "/envdash/v1/registrations/{id}", strings.NewReader(body))
+	req.SetPathValue("id", "")
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	h.replaceRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.Equal(t, "", gotID)
+	assert.False(t, setCalled)
+}
+
+func TestDeleteRegistration(t *testing.T) {
+	originGet := getRegistrationDoc
+	originDelete := deleteRegistrationDoc
+
+	var gotID string
+	var deleteCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		return nil
+	}
+
+	deleteRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotID = id
+		deleteCalled = true
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		deleteRegistrationDoc = originDelete
+	}()
+
+	h := &Handler{}
+
+	req := httptest.NewRequest(http.MethodDelete, "/envdash/v1/registrations/{id}", nil)
+	req.SetPathValue("id", "mock-id-001")
+	w := httptest.NewRecorder()
+
+	h.deleteRegistration(w, req)
+
+	resp := w.Result()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+	assert.True(t, deleteCalled)
+	assert.Equal(t, "mock-id-001", gotID)
+}
+
+func TestDeleteRegistrationGetFailure(t *testing.T) {
+	originGet := getRegistrationDoc
+	originDelete := deleteRegistrationDoc
+
+	var gotID string
+	var deleteCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotID = id
+		return errors.New("could not find registration")
+	}
+
+	deleteRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		deleteCalled = true
+		return nil
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		deleteRegistrationDoc = originDelete
+	}()
+
+	h := &Handler{}
+	req := httptest.NewRequest(http.MethodDelete, "/envdash/v1/registrations/{id}", nil)
+	req.SetPathValue("id", "mock-id-001")
+	w := httptest.NewRecorder()
+
+	h.deleteRegistration(w, req)
+
+	resp := w.Result()
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	assert.Equal(t, "mock-id-001", gotID)
+	assert.False(t, deleteCalled)
+}
+
+func TestDeleteRegistrationInvalidId(t *testing.T) {
+	originGet := getRegistrationDoc
+	originDelete := deleteRegistrationDoc
+
+	var gotID string
+	var gotDeleteID string
+	var deleteCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotID = id
+		return nil
+	}
+
+	deleteRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotDeleteID = id
+		deleteCalled = true
+		return errors.New("failed to delete registration")
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		deleteRegistrationDoc = originDelete
+	}()
+
+	h := &Handler{}
+	req := httptest.NewRequest(http.MethodDelete, "/envdash/v1/registrations/{id}", nil)
+	req.SetPathValue("id", "")
+	w := httptest.NewRecorder()
+
+	h.deleteRegistration(w, req)
+
+	resp := w.Result()
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.Equal(t, "", gotID)
+	assert.Equal(t, "", gotDeleteID)
+	assert.False(t, deleteCalled)
+}
+
+func TestDeleteRegistrationDeleteFailure(t *testing.T) {
+	originGet := getRegistrationDoc
+	originDelete := deleteRegistrationDoc
+
+	var gotID string
+	var gotDeleteID string
+	var deleteCalled bool
+
+	getRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotID = id
+		return nil
+	}
+
+	deleteRegistrationDoc = func(ctx context.Context, client *firestore.Client, id string) error {
+		gotDeleteID = id
+		deleteCalled = true
+		return errors.New("failed to delete registration")
+	}
+
+	defer func() {
+		getRegistrationDoc = originGet
+		deleteRegistrationDoc = originDelete
+	}()
+
+	h := &Handler{}
+	req := httptest.NewRequest(http.MethodDelete, "/envdash/v1/registrations/{id}", nil)
+	req.SetPathValue("id", "mock-id-001")
+	w := httptest.NewRecorder()
+
+	h.deleteRegistration(w, req)
+
+	resp := w.Result()
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(resp.Body)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	assert.Equal(t, "mock-id-001", gotID)
+	assert.Equal(t, "mock-id-001", gotDeleteID)
+	assert.True(t, deleteCalled)
 }
