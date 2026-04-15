a825fd5ce8eeb25281fcaf24b963c0ef6404ad9d
arpit <arpitsahoo2@gmail.com>
2026-04-15 15:21:38 +0200

    refactor: remove unused StatusPath constant from constants.go

--- internal/utility/constants.go
+++ internal/utility/constants.go
@@ -24,7 +24,6 @@ const ApplicationJSON = "application/json"
 const APIKeysCollection = "apiKeys"
 const AuthPath = "/envdash/v1/auth/"
 const AuthPathKey = "/envdash/v1/auth/{key}"
-const StatusPath = "/envdash/v1/status/"
 const APIKeyHeader = "Authorization"
 const BaseAPIKeyStarter = "sk-envdash-"
 
