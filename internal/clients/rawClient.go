0c72ae562e2c9ed92d6a6a0a2553f7b510812aae
arpit <arpitsahoo2@gmail.com>
2026-04-16 16:35:04 +0200

    feat: add RawClient implementation for API interactions

---
+++ internal/clients/rawClient.go
@@ -0,0 +1,31 @@
+package clients
+
+import (
+	"assignment-2/internal/models"
+	"context"
+)
+
+// RawClient is the concrete, default implementation of APIClient.
+// It currently has no fields, it groups the methods
+// and allows future extension.
+type RawClient struct{}
+
+// GetCountry delegates to FetchCountryInfoFunc.
+func (r *RawClient) GetCountry(ctx context.Context, iso string) (models.RestCountryResponse, error) {
+	return FetchCountryInfoFunc(iso)
+}
+
+// GetWeather delegates to FetchWeatherInfoFunc.
+func (r *RawClient) GetWeather(ctx context.Context, lat, lng float64) (models.OpenMeteoResponse, error) {
+	return FetchWeatherInfoFunc(lat, lng)
+}
+
+// GetExchangeRates delegates to FetchExchangeRateFunc.
+func (r *RawClient) GetExchangeRates(ctx context.Context, base string, targets []string) (map[string]float64, error) {
+	return FetchExchangeRateFunc(targets, base)
+}
+
+// GetAirQuality delegates to FetchAirQualityInfoFunc.
+func (r *RawClient) GetAirQuality(ctx context.Context, iso, capital string) (float64, float64, error) {
+	return FetchAirQualityInfoFunc(iso, capital)
+}
