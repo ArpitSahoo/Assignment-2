237b62b0d6774b781fabade682c6e3b22d6a26db
EiLi1 <183522504+eili1s@users.noreply.github.com.>
2026-04-16 17:40:31 +0200

    add: Rest Country name path

--- internal/utility/constants.go
+++ internal/utility/constants.go
@@ -21,12 +21,13 @@ const (
 
 // External API base URLs
 const (
-	RestCountriesAPIURL = "http://129.241.150.113:8080/v3.1/alpha/"
-	OpenMeteoAPIURL     = "https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lng}&hourly=temperature_2m,precipitation"
-	OpenAQURL           = "https://api.openaq.org/v3/locations?coordinates={lat},{lng}&radius=25000&parameters_id=1,2&iso={isoCode}&limit=100"
-	OpenAQLatestURL     = "https://api.openaq.org/v3/locations/{id}/latest"
-	OSMURL              = "https://nominatim.openstreetmap.org/search?city={cap}&countrycodes={isoCode}&format=jsonv2&addressdetails=1&limit=1"
-	CurrencyAPIURL      = "https://api.exchangerate-api.com/v4/latest/{cur}"
+	RestCountriesAPIURL     = "http://129.241.150.113:8080/v3.1/alpha/"
+	RestCountriesAPIURLName = "http://129.241.150.113:8080/v3.1/name/{name}"
+	OpenMeteoAPIURL         = "https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lng}&hourly=temperature_2m,precipitation"
+	OpenAQURL               = "https://api.openaq.org/v3/locations?coordinates={lat},{lng}&radius=25000&parameters_id=1,2&iso={isoCode}&limit=100"
+	OpenAQLatestURL         = "https://api.openaq.org/v3/locations/{id}/latest"
+	OSMURL                  = "https://nominatim.openstreetmap.org/search?city={cap}&countrycodes={isoCode}&format=jsonv2&addressdetails=1&limit=1"
+	CurrencyAPIURL          = "https://api.exchangerate-api.com/v4/latest/{cur}"
 )
 
 // URL template placeholders
